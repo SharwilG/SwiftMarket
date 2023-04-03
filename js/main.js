5b9dab7a3ae403f904373b15d58efb05a0939d50
SharwilG <109486956+SharwilG@users.noreply.github.com>
2023-04-03 20:21:49 +0530

    Add files via upload
    Hopefully Final Upload

--- js/main.js
+++ js/main.js
@@ -33,7 +33,31 @@
     });
 
 
-    
+    // Vendor carousel
+    $('.vendor-carousel').owlCarousel({
+        loop: true,
+        margin: 29,
+        nav: false,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+            0:{
+                items:2
+            },
+            576:{
+                items:3
+            },
+            768:{
+                items:4
+            },
+            992:{
+                items:5
+            },
+            1200:{
+                items:6
+            }
+        }
+    });
 
 
     // Related carousel
